a5408c7bb3aadf1513ec59d0a2ab766920abe689
danielecolaci <danielecolaci@outlook.com>
2024-02-21 16:02:19 +0100

    Add FizzBuzz in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
-console.log('ok');
-
 const list = document.getElementById('list');
 console.log(list);
 
+//Stampare i numeri da 1 a 100 in console
 for (let i = 1; i <= 100; i++) {
     console.log(i);
+    //Stampare FizzBuzz in console
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log(i + 'FizzBuzz');
+    }
+    else if (i % 3 === 0) {
+        console.log(i + 'Fizz');
+    }
+    else if (i % 5 === 0) {
+        console.log(i + 'Buzz');
+    }
+
 }
