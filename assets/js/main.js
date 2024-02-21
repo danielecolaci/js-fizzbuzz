ab3645f07aa3311dc5ab52737f7fae81abaf7cf1
danielecolaci <danielecolaci@outlook.com>
2024-02-21 16:08:58 +0100

    Print number on html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,28 @@
 const list = document.getElementById('list');
 console.log(list);
 
+let number = '';
+
 //Stampare i numeri da 1 a 100 in console
 for (let i = 1; i <= 100; i++) {
     console.log(i);
     //Stampare FizzBuzz in console
     if ((i % 3 === 0) && (i % 5 === 0)) {
         console.log(i + 'FizzBuzz');
+        number += '<li>FizzBuzz</li>';
     }
     else if (i % 3 === 0) {
         console.log(i + 'Fizz');
+        number += '<li>Fizz</li>';
     }
     else if (i % 5 === 0) {
         console.log(i + 'Buzz');
+        number += '<li>Buzz</li>';
+    }
+    else {
+        number += `<li>${i}</li>`;
     }
 
 }
+
+list.innerHTML = number;
