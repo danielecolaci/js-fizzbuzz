41782713165439070f3001e4baf84333d04e0902
danielecolaci <danielecolaci@outlook.com>
2024-02-21 16:32:04 +0100

    Add Style

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,17 @@ let number = '';
 //Stampare i numeri da 1 a 100 in console
 for (let i = 1; i <= 100; i++) {
     console.log(i);
-    //Stampare FizzBuzz in console
+    //Stampare FizzBuzz in console e in pagina
     if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log(i + 'FizzBuzz');
+        //console.log(i + 'FizzBuzz');
         number += '<li>FizzBuzz</li>';
     }
     else if (i % 3 === 0) {
-        console.log(i + 'Fizz');
+        //console.log(i + 'Fizz');
         number += '<li>Fizz</li>';
     }
     else if (i % 5 === 0) {
-        console.log(i + 'Buzz');
+        //console.log(i + 'Buzz');
         number += '<li>Buzz</li>';
     }
     else {
